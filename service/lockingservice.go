561601fa058476be4d2c64e5a86b75322610c25e
Matías Torres <torresmat@gmail.com>
2024-10-23 16:44:09 +0200

    fix(locking): fixes race condition on locking service

--- service/lockingservice.go
+++ service/lockingservice.go
@@ -1,6 +1,9 @@
 package service
 
-import "sync"
+import (
+	"log/slog"
+	"sync"
+)
 
 // If we want to scale horizontally the application, we are gonna be needing
 // a external service for locking (like Redis) so 2 different processes don't
@@ -14,33 +17,37 @@ type LockService interface {
 }
 
 type VolatileLockService struct {
-	deviceMutexes map[string]*sync.RWMutex
-	generalMutex  sync.RWMutex
+	deviceMutexes map[string]*sync.Mutex
+	generalMutex  sync.Mutex
 }
 
 // To be called while signing data before modiying the `lastSignature`
 // and `signatureCounter` fields
 func (lockService *VolatileLockService) Lock(deviceId string) {
 	lockService.generalMutex.Lock()
-	defer lockService.generalMutex.Unlock()
-
 	if _, ok := lockService.deviceMutexes[deviceId]; !ok {
-		lockService.deviceMutexes[deviceId] = &sync.RWMutex{}
+		lockService.deviceMutexes[deviceId] = &sync.Mutex{}
 	}
+	lockService.generalMutex.Unlock()
+
 	lockService.deviceMutexes[deviceId].Lock()
+	slog.Debug("lock acquired for device", "deviceId", deviceId)
 }
 
 // After successfull save in databaseof the device (and the signature),
 // we would need to unlock the device so other threads/processes can modify it.
 func (lockService *VolatileLockService) Unlock(deviceId string) {
-	lockService.generalMutex.Lock()
-	defer lockService.generalMutex.Unlock()
-
-	lockService.deviceMutexes[deviceId].Unlock()
+	mutex, ok := lockService.deviceMutexes[deviceId]
+	if !ok {
+		slog.Warn("attempting to unlock a non existent lock for device", "deviceId", deviceId)
+		return
+	}
+	mutex.Unlock()
+	slog.Debug("lock released for device", "deviceId", deviceId)
 }
 
 func NewVolatileLockService() *VolatileLockService {
 	return &VolatileLockService{
-		deviceMutexes: make(map[string]*sync.RWMutex),
+		deviceMutexes: make(map[string]*sync.Mutex),
 	}
 }
